1033203cab0fe50da27969f3624cfc435baf164b
RajShekhorRoy <rajshekhorroy@gmail.com>
2019-09-25 20:37:45 -0500

    problem 2 with 98 % accuracy

--- assignment3_problem_2.py
+++ assignment3_problem_2.py
@@ -5,7 +5,7 @@ import csv
 
 LEARNING_RATE = 0.0001
 
-ITERATION = 1000
+ITERATION = 10000
 initial_a = 1
 initial_b = 0
 initial_c = 0
@@ -27,15 +27,19 @@ def gradient_descent_runner(_a, _b, _c, _d, _e, df):
     return [a, b, c, d, e]
 
 
-def get_cost(_a, _b, _c, _d, _e, x):
+def sigmod_function(_a, _b, _c, _d, _e, x):
     cost = 1 / (1 + math.exp(-1 * (_a + _b * x[0] + _c * x[1] + _d * x[2] + _e * x[3])))
-    print('Cost_'+str(cost))
-    cost = x[4] - cost
-    return cost
+    # print('Cost_'+str(cost))
+    # cost = x[4] - cost
+    print(cost)
+    if cost >= 0.5:
+        return 1
+    elif cost < 0.5:
+        return 0
 
 
 def gradient_descent_step(_a, _b, _c, _d, _e, df):
-    a_gradient = 0
+    a_gradient = 1
     b_gradient = 0
     c_gradient = 0
     d_gradient = 0
@@ -45,15 +49,21 @@ def gradient_descent_step(_a, _b, _c, _d, _e, df):
     # find gradient of the cost function
     # cost = get_cost(_a, _b, _c, _d, _e, x)
     for x in df:
-        cost = float(get_cost(_a, _b, _c, _d, _e, x))
+        cost = float(sigmod_function(_a, _b, _c, _d, _e, x))
         # print(cost)
 
         # print(cost)
-        a_gradient += (1 / length) * cost
-        b_gradient += (1 / length) * cost * (x[0])
-        c_gradient += (1 / length) * cost * (x[1])
-        d_gradient += (1 / length) * cost * (x[2])
-        e_gradient += (1 / length) * cost * (x[3])
+        # a_gradient += (1 / length) * (sigmod_function(1) - x[4])
+        # b_gradient += (1 / length) * (sigmod_function(x[0] * _b) - x[4]) * (x[0])
+        # c_gradient += (1 / length) * (sigmod_function(x[1] * _c) - x[4]) * (x[1])
+        # d_gradient += (1 / length) * (sigmod_function(x[2] * _d) - x[4]) * (x[2])
+        # e_gradient += (1 / length) * (sigmod_function(x[3] * _e) - x[4]) * (x[3])
+
+        a_gradient += (1 / length) * (cost - x[4])
+        b_gradient += (1 / length) * (cost - x[4]) * (x[0])
+        c_gradient += (1 / length) * (cost - x[4]) * (x[1])
+        d_gradient += (1 / length) * (cost - x[4]) * (x[2])
+        e_gradient += (1 / length) * (cost - x[4]) * (x[3])
 
         i = i + 1
     # print('a_gradient ' + str(a_gradient) + ' b_gradient ' + str(b_gradient) + ' c_gradient ' + str(c_gradient))
@@ -68,25 +78,35 @@ def gradient_descent_step(_a, _b, _c, _d, _e, df):
 
 def predict(_a, _b, _c, _d, _e, x):
     val = float(_a + _b * x[0] + _c * x[1] + _d * x[2] + _e * x[3])
-    return val
+    cost = 1 / (1 + math.exp(-1 * val))
+    return cost
 
 
 def accuracy(_a, _b, _c, _d, _e, array):
     total_accuracy = 0
     result_array = []
+    number_of_one = 0
+    correct_0 = 0
+    correct_1 = 0
+    print('total  ' + str(len(array)))
+
     for x in array:
         val = float(_a + _b * x[0] + _c * x[1] + _d * x[2] + _e * x[3])
-        if val > 0.5:
-            result_array.append(1)
-        else:
-            result_array.append(0)
-    i = 0
+        cost = 1 / (1 + math.exp(-1 * val))
 
-    for res in result_array:
-        if res == array[i][4]:
-            total_accuracy += 1
-        i = i + 1
-    return (total_accuracy * 100) / len(result_array)
+        if cost < 0.5:
+            if x[4] == 0:
+                correct_0 = correct_0 + 1
+        if cost >= 0.5:
+            if x[4] == 1:
+                correct_1 = correct_1 + 1
+        if x[4] == 1:
+            number_of_one += 1
+    print('true number of 1 ' + str(number_of_one)+ '----' + str(correct_1))
+
+    print('true number of 0 ' + str(len(array) - number_of_one) + '----' + str(correct_0))
+
+    return (100*float(correct_1+correct_0))/len(array)
 
 
 def data_reader():
@@ -110,9 +130,10 @@ a, b, c, d, e = 0, 0, 0, 0, 0
 a, b, c, d, e = gradient_descent_runner(initial_a, initial_b, initial_c, initial_d, initial_e, data_array)
 
 print(a, b, c, d, e)
-print(data_array[22])
-print(predict(a, b, c, d, e, data_array[22]))
-# print('accuracy ' + str(accuracy(a, b, c, d, e, data_array)))
+test_no = 1
+# print(data_array[test_no])
+# print(predict(a, b, c, d, e, data_array[test_no]))
+print('accuracy ' + str(accuracy(a, b, c, d, e, data_array)))
 #
 # plt.scatter(x, y, color='blue')
 #
