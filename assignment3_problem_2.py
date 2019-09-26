07ea95e3b09816640ac4c2bd7cdc7601585f5f98
RajShekhorRoy <rajshekhorroy@gmail.com>
2019-09-25 19:04:29 -0500

    try later

--- assignment3_problem_2.py
+++ assignment3_problem_2.py
@@ -5,7 +5,7 @@ import csv
 
 LEARNING_RATE = 0.0001
 
-ITERATION = 10000
+ITERATION = 1000
 initial_a = 1
 initial_b = 0
 initial_c = 0
@@ -29,6 +29,7 @@ def gradient_descent_runner(_a, _b, _c, _d, _e, df):
 
 def get_cost(_a, _b, _c, _d, _e, x):
     cost = 1 / (1 + math.exp(-1 * (_a + _b * x[0] + _c * x[1] + _d * x[2] + _e * x[3])))
+    print('Cost_'+str(cost))
     cost = x[4] - cost
     return cost
 
@@ -46,13 +47,13 @@ def gradient_descent_step(_a, _b, _c, _d, _e, df):
     for x in df:
         cost = float(get_cost(_a, _b, _c, _d, _e, x))
         # print(cost)
-        cost = cost * cost
+
         # print(cost)
-        a_gradient += (2 / length) * cost * (-1)
-        b_gradient += (2 / length) * cost * (-1)
-        c_gradient += (2 / length) * cost * (-1)
-        d_gradient += (2 / length) * cost * (-1)
-        e_gradient += (2 / length) * cost * (-1)
+        a_gradient += (1 / length) * cost
+        b_gradient += (1 / length) * cost * (x[0])
+        c_gradient += (1 / length) * cost * (x[1])
+        d_gradient += (1 / length) * cost * (x[2])
+        e_gradient += (1 / length) * cost * (x[3])
 
         i = i + 1
     # print('a_gradient ' + str(a_gradient) + ' b_gradient ' + str(b_gradient) + ' c_gradient ' + str(c_gradient))
@@ -64,13 +65,10 @@ def gradient_descent_step(_a, _b, _c, _d, _e, df):
     new_e = _e - LEARNING_RATE * d_gradient
     return [new_a, new_b, new_c, new_d, new_e]
 
-#
-# def predict(_a, _b, _c, array):
-#     result = []
-#     for x in array:
-#         val = float(_a + _b * x + _c * (x ** 2))
-#         result.append(val)
-#     return result
+
+def predict(_a, _b, _c, _d, _e, x):
+    val = float(_a + _b * x[0] + _c * x[1] + _d * x[2] + _e * x[3])
+    return val
 
 
 def accuracy(_a, _b, _c, _d, _e, array):
@@ -78,7 +76,6 @@ def accuracy(_a, _b, _c, _d, _e, array):
     result_array = []
     for x in array:
         val = float(_a + _b * x[0] + _c * x[1] + _d * x[2] + _e * x[3])
-        print(val)
         if val > 0.5:
             result_array.append(1)
         else:
@@ -92,26 +89,30 @@ def accuracy(_a, _b, _c, _d, _e, array):
     return (total_accuracy * 100) / len(result_array)
 
 
-data_dir = '/home/rajroy/Desktop/iris.data'
-data_array = []
-with open(data_dir) as csv_file:
-    reader = csv.reader(csv_file)  # change contents to floats
-    for row in reader:  # each row is a list
-        value = 0
-        if row[4] == 'Iris-setosa':
+def data_reader():
+    data_dir = '/home/rajroy/Desktop/iris.data'
+    data_array = []
+    with open(data_dir) as csv_file:
+        reader = csv.reader(csv_file)  # change contents to floats
+        for row in reader:  # each row is a list
             value = 0
-        elif row[4] == 'Iris-versicolor':
-            value = 1
-        data_array.append([float(row[0]), float(row[1]), float(row[2]), float(row[3]), value])
+            if row[4] == 'Iris-setosa':
+                value = 0
+            elif row[4] == 'Iris-versicolor':
+                value = 1
+            data_array.append([float(row[0]), float(row[1]), float(row[2]), float(row[3]), value])
 
-print(data_array)
+    return data_array
 
+
+data_array = data_reader()
 a, b, c, d, e = 0, 0, 0, 0, 0
 a, b, c, d, e = gradient_descent_runner(initial_a, initial_b, initial_c, initial_d, initial_e, data_array)
 
 print(a, b, c, d, e)
-
-print('accuracy '+str(accuracy(a, b, c, d, e, data_array)))
+print(data_array[22])
+print(predict(a, b, c, d, e, data_array[22]))
+# print('accuracy ' + str(accuracy(a, b, c, d, e, data_array)))
 #
 # plt.scatter(x, y, color='blue')
 #
